deb92f8404f9e27ef5e0aa26d7fcb4ed73fb19e5
Eliza Brock <eliza@elizabrocksoftware.com>
2013-05-16 09:43:42 -0500

    Extremely basic add functionality

--- test/test_adding_project.rb
+++ test/test_adding_project.rb
@@ -2,12 +2,13 @@ require 'test_helper'
 
 class TestAddingProject < Test::Unit::TestCase
   def test_takes_arguments_and_saves_them
-    # start with no projects
-    assert_equal Project.count, 0
-    # `ruby futureperfect add foo`
-    project = Project.create( name: 'foo' )
-    # project => #<Project:12312 @name='foo'>
-    # end up with a 'foo' project
-    assert_equal Project.count, 1
+    assert_equal 0, Project.count
+    puts `ruby futureperfect add foo`
+    assert_equal 1, Project.count
+  end
+
+  def test_takes_arguments_and_uses_them
+    puts `ruby futureperfect add foo`
+    assert_equal 'foo', Project.last.name
   end
 end
