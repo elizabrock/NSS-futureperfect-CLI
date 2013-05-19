214d7b605e92c7827c94fda28335b8c2b86b2214
Eliza Brock <eliza@elizabrocksoftware.com>
2013-05-19 13:03:50 -0500

    Give users feedback on missing commands

---
+++ test/integration/test_commands_that_dont_exist.rb
@@ -0,0 +1,31 @@
+require_relative '../test_helper'
+
+class TestCommandsThatDontExist < MiniTest::Unit::TestCase
+  include DatabaseCleaner
+
+  def test_help_message_is_printed
+    output = `ruby futureperfect foo remove`
+    ["list", "add", "remove", "start"].each do |command|
+      assert output.include?(command), "Output was '#{output}', but should have included '#{command}'"
+    end
+  end
+
+  def test_user_sees_error_message
+    output = `ruby futureperfect foo foo`
+    message = "FuturePerfect does not (yet?) support the 'foo' command."
+    assert output.include?(message), "Output was '#{output}', but should have included '#{message}'"
+  end
+
+  def test_help_message_is_printed_with_help_command
+    output = `ruby futureperfect foo help`
+    ["list", "add", "remove", "start"].each do |command|
+      assert output.include?(command), "Output was '#{output}', but should have included '#{command}'"
+    end
+  end
+
+  def test_help_command_doesnt_print_error_message
+    output = `ruby futureperfect help`
+    message = "FuturePerfect does not (yet?) support"
+    assert !output.include?(message), "Output was '#{output}', but should have included '#{message}'"
+  end
+end
